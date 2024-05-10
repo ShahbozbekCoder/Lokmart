36df1f41c23b776593385589db0d07a29ac6cdcf
ShahbozbekCoder <shahbozbey99@gmail.com>
2024-05-10 17:28:43 +0500

    last update 10.05.2024

--- BottomNavigation/app/src/main/java/com/example/bottomnavigation/bottom_navigation/MyBottomNavigation.kt
+++ BottomNavigation/app/src/main/java/com/example/bottomnavigation/bottom_navigation/MyBottomNavigation.kt
@@ -1,11 +1,17 @@
 package com.example.bottomnavigation.bottom_navigation
 
 import androidx.compose.foundation.background
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
@@ -24,5 +30,24 @@ fun MyBottomNavigation(
     ) {
         val backStackEntry by navController.currentBackStackEntryAsState()
         val currentRout = backStackEntry?.destination?.route
+        listItems.forEach { item ->
+            NavigationBarItem(
+                selected = currentRout == item.route,
+                onClick = {
+                    navController.navigate(item.route)
+return@NavigationBarItem
+                },
+                icon = {
+                    Icon(painter = painterResource(id = item.iconID), contentDescription = "icon1")
+                }, label = {
+                    Text(text = item.title, fontSize = 9.sp)
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.Red,
+                    unselectedIconColor = Color.Gray
+                )
+            )
+
+        }
     }
 }
